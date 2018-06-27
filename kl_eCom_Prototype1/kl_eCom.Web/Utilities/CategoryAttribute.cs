38e2b400a5293eeb7ac76a0a74ae42b3c50fe8a5
Rishant Dutt <kirarishant@outlook.com>
2018-06-27 13:03:58 +0530

    Attribute type support added, for filtering.

--- kl_eCom_Prototype1/kl_eCom.Web/Utilities/CategoryAttribute.cs
+++ kl_eCom_Prototype1/kl_eCom.Web/Utilities/CategoryAttribute.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -15,8 +16,17 @@ namespace kl_eCom.Web.Utilities
         [Required]
         public string Name { get; set; }
 
+        public InformationType InfoType { get; set; }
+
         [Required]
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
+    }
+
+    public enum InformationType
+    {
+        Other,
+        Numerical,
+        String
     }
 }
